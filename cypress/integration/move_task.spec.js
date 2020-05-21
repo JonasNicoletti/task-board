a13116ee4b2a73922020c37cbcb57d68c22ac569
Jonas Nicoletti <sjnao89@gmail.com>
2020-05-21 19:03:14 +0200

    (test) drag and drop

--- cypress/integration/move_task.spec.js
+++ cypress/integration/move_task.spec.js
@@ -126,4 +126,75 @@ describe('move task', () => {
       .children()
       .should('have.length', 1)
   })
+
+  it.only('move task using drag and drop', () => {
+    const dataTransfer = new DndSimulatorDataTransfer()
+
+    cy
+      .get('#dashboard')
+      .children()
+      .first()
+      .children()
+      .first()
+      .next()
+      .children()
+      .first()
+      .trigger('mousedown', { which: 1 })
+      .trigger('dragstart', { dataTransfer })
+      .trigger('drag', {});
+
+    cy
+      .get('#dashboard')
+      .children()
+      .first()
+      .next()
+      .next()
+      .children()
+      .first()
+      .next()
+      .trigger('dragover', { dataTransfer })
+      .trigger('drop', { dataTransfer })
+      .trigger('dragend', { dataTransfer })
+      .trigger('mouseup', { which: 1 })
+
+
+
+
+
+    function DndSimulatorDataTransfer() {
+      this.data = {}
+    }
+
+    DndSimulatorDataTransfer.prototype.dropEffect = "move"
+    DndSimulatorDataTransfer.prototype.effectAllowed = "all"
+    DndSimulatorDataTransfer.prototype.files = []
+    DndSimulatorDataTransfer.prototype.items = []
+    DndSimulatorDataTransfer.prototype.types = []
+
+    DndSimulatorDataTransfer.prototype.clearData = function (format) {
+      if (format) {
+        delete this.data[format]
+
+        const index = this.types.indexOf(format)
+        delete this.types[index]
+        delete this.data[index]
+      } else {
+        this.data = {}
+      }
+    }
+
+    DndSimulatorDataTransfer.prototype.setData = function (format, data) {
+      this.data[format] = data
+      this.items.push(data)
+      this.types.push(format)
+    }
+
+    DndSimulatorDataTransfer.prototype.getData = function (format) {
+      if (format in this.data) {
+        return this.data[format]
+      }
+
+      return ""
+    }
+  })
 })
