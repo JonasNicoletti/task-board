e9e684bc35ede33e012fae4a15776066d3623e7f
Jonas Nicoletti <sjnao89@gmail.com>
2020-06-01 09:35:08 +0200

    (feat) upgrade dependency

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { MuiThemeProvider, responsiveFontSizes } from '@material-ui/core/styles'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  MuiThemeProvider,
+  responsiveFontSizes,
+} from "@material-ui/core/styles";
+import { Provider } from "react-redux";
 
-import './index.css'
-import App from './App'
-import taskReducer from './store/reducers/taskReducer'
-import { createStore } from 'redux'
+import "./index.css";
+import App from "./App";
+import taskReducer from "./store/reducers/taskReducer";
+import { createStore } from "redux";
 
-import * as serviceWorker from './serviceWorker'
-import theme from './theme'
-import { BrowserRouter } from 'react-router-dom'
-import { CssBaseline } from '@material-ui/core'
-import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
+import * as serviceWorker from "./serviceWorker";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-
-const store = createStore(
-  taskReducer
-)
+const store = createStore(taskReducer);
 
 const app = (
   <Provider store={store}>
-    <DndProvider backend={Backend}>
+    <DndProvider backend={HTML5Backend}>
       <BrowserRouter basename="/task-board">
         <MuiThemeProvider theme={responsiveFontSizes(theme)}>
           <CssBaseline />
@@ -31,10 +31,10 @@ const app = (
       </BrowserRouter>
     </DndProvider>
   </Provider>
-)
+);
 
-ReactDOM.render(app, document.getElementById('root'))
+ReactDOM.render(app, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
