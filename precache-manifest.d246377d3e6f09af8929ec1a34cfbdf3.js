c9689073ae3c653be5e08ffc5dff3e8f07bccfcd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-10 21:28:06 +0000

    Deploy JonasNicoletti/task-board to github.com/JonasNicoletti/task-board.git:gh-pages

---
+++ precache-manifest.d246377d3e6f09af8929ec1a34cfbdf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774a620db406fac15fb654422f931daa",
+    "url": "/task-board/index.html"
+  },
+  {
+    "revision": "e6e713ff219bcfd33846",
+    "url": "/task-board/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30f2a612165312ca9804",
+    "url": "/task-board/static/js/2.ed9eadad.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/task-board/static/js/2.ed9eadad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e713ff219bcfd33846",
+    "url": "/task-board/static/js/main.0ce1237e.chunk.js"
+  },
+  {
+    "revision": "d4c5b0105149d72f8a6d",
+    "url": "/task-board/static/js/runtime-main.9a891cbb.js"
+  }
+]);
